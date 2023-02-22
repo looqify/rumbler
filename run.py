d83d0707f8b1cd1ae6f60369fcb6e5ebfa30b329
Lucky Men <looqifi@gmail.com>
2023-02-22 08:32:38 +0700

    add fakeua

--- run.py
+++ run.py
@@ -2,21 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options
+from fake_useragent import UserAgent
 import time
 import sys
+import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
-
-target = None
+ua = UserAgent()
+target, proxies = None, []
 
 
 def play(thread_no, proxy):
-    PROXY = proxy
-    webdriver.DesiredCapabilities.FIREFOX['proxy'] = {
-        "httpProxy": PROXY,
-        "sslProxy": PROXY,
-        "proxyType": "MANUAL",
-    }
-    browser = webdriver.Firefox()
+    if proxy and ':' in proxy:
+        webdriver.DesiredCapabilities.FIREFOX['proxy'] = {
+            "httpProxy": proxy,
+            "sslProxy": proxy,
+            "proxyType": "MANUAL",
+        }
+    options = Options()
+    options.set_preference("general.useragent.override", ua.random)
+    browser = webdriver.Firefox(options=options)
+    browser.get("https://rumble.com")
+    time.sleep(5)
     browser.get(target)
     play_selector = 'div.bigPlayUI.ctp > div'
     WebDriverWait(browser, 20).until(
@@ -51,19 +58,36 @@ def cekip(thread_no, proxy):
     return f'\n Thread no {thread_no} done!'
 
 
+def whoer():
+    options = Options()
+    useragent = ua.random
+    print(useragent)
+    options.set_preference("general.useragent.override", useragent)
+    browser = webdriver.Firefox(options=options)
+    browser.get("https://whoer.net")
+    time.sleep(50)
+    browser.quit()
+
+
 def main():
-    global target
+    global target, proxies
     cek = input('Cek proxy? y/n : ')
     if cek.lower() != 'y':
         LINK = input('Link : ')
         target = LINK
     WORKERS = input('Workers : ') or 4
-    proxies = open('proxies.txt', 'r').readlines()
+    PROX = input('Proxy url : ')
+    if (PROX != '' and 'http' in PROX):
+        res = requests.get(PROX)
+        res = res.text
+        proxies = res.splitlines()
+    else:
+        proxies = open('proxies.txt', 'r').readlines()
     if len(proxies) < 1:
         print('Proxy tidak boleh kosong!')
         sys.exit('PROXY_EMPTY')
 
-    with ThreadPoolExecutor(max_workers=WORKERS) as executor:
+    with ThreadPoolExecutor(max_workers=int(WORKERS)) as executor:
         futures = [executor.submit(cekip if cek.lower(
         ) == 'y' else play, i, p) for i, p in enumerate(proxies)]
         for future in as_completed(futures):
@@ -73,3 +97,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    # whoer()
